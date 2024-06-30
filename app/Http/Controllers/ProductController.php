c13a0f46454b03d229e5145d756745b90333a7fa
Nischal <nischalshrestha800@gmail.com>
2024-06-30 15:35:47 +0545

    Backend Code of Crud App

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -16,6 +17,14 @@ class ProductController extends Controller
     }
     public function store(Request $request)
     {
-        dd($request);
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:2',
+            'description' => 'nullable'
+        ]);
+
+        $newProduct = Product::create($data);
+        return redirect(route('product.index'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/', function () {
 });
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product', [ProductController::class, 'create'])->name('product.store');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
